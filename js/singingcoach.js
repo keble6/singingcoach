8ee87417261a1934596d32363331b8a823cb7644
Rob Wilson <rob@rwilson.org.uk>
2020-04-08 15:46:45 +0100

    updated with scale
    Need to work on 8 oscillators

--- js/singingcoach.js
+++ js/singingcoach.js
@@ -96,6 +96,92 @@ function gotStream(stream) {
     mediaStreamSource.connect( analyser );
     updatePitch();
 }
+
+/********************* Scale functions *********************/
+function playNote(startTime, duration, pitch) { //new oscillator created for every note (allows an effective restart)
+  const oscillator = audioContext.createOscillator();
+  
+  analyser = audioContext.createAnalyser();
+  analyser.fftSize = 2048;
+  oscillator.connect(analyser);
+  analyser.connect(audioContext.destination);
+  oscillator.frequency.value = pitch;
+  oscillator.type = 'sine';
+  oscillator.start(startTime);
+  oscillator.stop(startTime + duration);
+}
+
+/*************** notes ************/
+const notes = {
+    C0: 16.351, Db0: 17.324, D0: 18.354, Eb0: 19.445, E0: 20.601, F0: 21.827, Gb0: 23.124, G0: 24.499, Ab0: 25.956, A0: 27.5, Bb0: 29.135, B0: 30.868,
+    C1: 32.703, Db1: 34.648, D1: 36.708, Eb1: 38.891, E1: 41.203, F1: 43.654, Gb1: 46.249, G1: 48.999, Ab1: 51.913, A1: 55, Bb1: 58.27, B1: 61.735,
+    C2: 65.406, Db2: 69.296, D2: 73.416, Eb2: 77.782, E2: 82.407, F2: 87.307, Gb2: 92.499, G2: 97.999, Ab2: 103.826, A2: 110, Bb2: 116.541, B2: 123.471,
+    C3: 130.813, Db3: 138.591, D3: 146.832, Eb3: 155.563, E3: 164.814, F3: 174.614, Gb3: 184.997, G3: 195.998, Ab3: 207.652, A3: 220, Bb3: 233.082, B3: 246.942,
+    C4: 261.626, Db4: 277.183, D4: 293.665, Eb4: 311.127, E4: 329.628, F4: 349.228, Gb4: 369.994, G4: 391.995, Ab4: 415.305, A4: 440, Bb4: 466.164, B4: 493.883,
+    C5: 523.251, Db5: 554.365, D5: 587.33, Eb5: 622.254, E5: 659.255, F5: 698.456, Gb5: 739.989, G5: 783.991, Ab5: 830.609, A5: 880, Bb5: 932.328, B5: 987.767,
+    C6: 1046.502, Db6: 1108.731, D6: 1174.659, Eb6: 1244.508, E6: 1318.51, F6: 1396.913, Gb6: 1479.978, G6: 1567.982, Ab6: 1661.219, A6: 1760, Bb6: 1864.655, B6: 1975.533,
+    C7: 2093.005, Db7: 2217.461, D7: 2349.318, Eb7: 2489.016, E7: 2637.021, F7: 2793.826, Gb7: 2959.955, G7: 3135.964, Ab7: 3322.438, A7: 3520, Bb7: 3729.31, B7: 3951.066
+};
+
+function toggleScale() {
+  
+  //new plan: 8 oscillators, then a gainNode conected to them in sequence
+  audioContext = new AudioContext();
+  
+  if (isPlaying) {
+    analyser = null;
+    isPlaying = false;
+    return "play oscillator";
+  }
+  else {
+    const tempo = 20;
+    const secondsPerBeat = 60 / tempo;
+    const secondsPerBar = secondsPerBeat * 4;
+    const secondsPerTick = secondsPerBeat / 4;
+    const scaleNotes = [notes.C4,notes.D4,notes.E4,notes.F4,notes.G4,notes.A4,notes.B4,notes.C5];
+    
+    var osc[0] = audioContext.createOscillator();
+    var osc[1] = audioContext.createOscillator();
+    var osc[2] = audioContext.createOscillator();
+    var osc[3] = audioContext.createOscillator();
+    var osc[4] = audioContext.createOscillator();
+    var osc[5] = audioContext.createOscillator();
+    var osc[6] = audioContext.createOscillator();
+    var osc[7] = audioContext.createOscillator();
+
+    for(i=0; i<8; i++){
+      osc[i].frequency.value = notes.scaleNotes[i].pitch;
+      console.log(notes.scaleNotes[i].pitch);
+    }
+    /*
+    let oscillator = audioContext.createOscillator();
+    oscillator.frequency.value = 493.88; //B4
+
+    let modulator = audioContext.createOscillator();
+    modulator.frequency.value=0.1;
+    modulator.type = 'triangle';
+
+    let modulationGain = audioContext.createGain();
+    modulationGain.gain.value  = 100;
+
+  
+    modulator.connect(modulationGain);
+    modulationGain.connect(oscillator.frequency);
+    oscillator.connect(analyser);
+
+    oscillator.start(0);
+    modulator.start(0);
+    
+    analyser.connect( audioContext.destination );
+    //sourceNode.start(0);*/
+  
+    isPlaying = true;
+    isLiveInput = false;
+    updatePitch();
+
+    return "stop";
+    }
+}
 /************************************************ */
 
 function toggleOscillator() {
